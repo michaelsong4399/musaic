20029364d0c4657ef5f235ed7e018b52d4898ada
Yihong Song <ys377@duke.edu>
2023-01-25 08:40:06 -0500

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,8 @@ import { useRouter } from "next/router";
 export default function Test() {
     function handleClick() {
         var client_id = "a9c70b25de314478a9a92f9cc63a0a35";
-        var redirect_uri = "http://localhost:3000/join";
+        // var redirect_uri = "http://localhost:3000/join";
+        var redirect_uri = "https://musaicduke.vercel.app/join";
         var scope = "user-read-private user-read-email";
 
         var url = "https://accounts.spotify.com/authorize";
