ec900e2041e787f16b466d1bbbcf5b6b9ef564f8
Yihong Song <ys377@duke.edu>
2023-01-25 14:43:26 -0500

    Update Packages

--- pages/party/[pid]/host.js
+++ pages/party/[pid]/host.js
@@ -1,15 +1,7 @@
-import SpotifyPlayer from "react-spotify-web-playback";
-
 export default function Host() {
     return (
         <>
             <div>Host</div>
-            <SpotifyPlayer
-                token="BQB_BDeFTTErV7Qo0gUItFmG5PrmaWoEjeoCaBmzM9rctPSDjCHd8lFZ9dpTbuUkvp1uAT_3i4rg8_gHLgt35fG8I5cK-tCp-lWa37cUBGurVoeuojQIUA53QxAQlcNa0lmsRUj9IiG07odLB_zwTilyQAs_lKrNobpMk0rHSQgK1dWlyogFKfOQ_bvpRaXGxupPeesB7_5A3Md_tfdb2efyy9_0xpOkK5gpMW0Ws4YUEdkjk6k
-                "
-                uris={["spotify:artist:6HQYnRM4OzToCYPpVBInuU"]}
-            />
-            ;
         </>
     );
 }
