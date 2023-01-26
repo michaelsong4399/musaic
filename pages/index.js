b02ccfb6ef27d556c0d354ed9aa27515747966c9
Yihong Song <ys377@duke.edu>
2023-01-26 14:28:44 -0500

    Update Start Page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,34 @@ import { useRouter } from "next/router";
 
 export default function Home() {
     const router = useRouter();
+    function handleClick() {
+        var client_id = "a9c70b25de314478a9a92f9cc63a0a35";
+        // var redirect_uri = "http://localhost:3000/join";
+        var redirect_uri = "https://musaicduke.vercel.app/join";
+        var scope =
+            "user-read-private user-read-email playlist-read-private user-library-read";
+
+        var url = "https://accounts.spotify.com/authorize";
+        url += "?response_type=token";
+        url += "&client_id=" + encodeURIComponent(client_id);
+        url += "&scope=" + encodeURIComponent(scope);
+        url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+        url += "&state=CUBE";
+        console.log(url);
+        router.push(url);
+    }
     return (
         <>
-            <div
-                onClick={() => {
-                    router.push("/test");
-                }}>
-                Host
+            <div className="error-page">
+                <img className="error-logo" src="/logo.svg"></img>
+                <div className="error-text">Cube's Party</div>
+                <div
+                    className="error-text start-button"
+                    onClick={() => {
+                        handleClick();
+                    }}>
+                    Join
+                </div>
             </div>
         </>
     );
--- pages/test.js
+++ pages/test.js
@@ -3,8 +3,8 @@ import { useRouter } from "next/router";
 export default function Test() {
     function handleClick() {
         var client_id = "a9c70b25de314478a9a92f9cc63a0a35";
-        var redirect_uri = "http://localhost:3000/join";
-        // var redirect_uri = "https://musaicduke.vercel.app/join";
+        // var redirect_uri = "http://localhost:3000/join";
+        var redirect_uri = "https://musaicduke.vercel.app/join";
         var scope =
             "user-read-private user-read-email playlist-read-private user-library-read";
 
